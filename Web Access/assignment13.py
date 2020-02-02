2f1661def03af2a34a03667cde08373c288a9697
Atharva1992 <atharvakekare@gmail.com>
2020-02-02 14:24:56 +0530

    Added span tag instead of td in assignment13.py

--- Web Access/assignment13.py
+++ Web Access/assignment13.py
@@ -1,13 +1,8 @@
-# To run this, download the BeautifulSoup zip file
-# http://www.py4e.com/code3/bs4.zip
-# and unzip it in the same directory as this file
-
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 import re
 
-# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
@@ -16,18 +11,20 @@ url = "http://py4e-data.dr-chuck.net/comments_42.html"
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 numbers = list()
-# Retrieve all of the anchor tags
-tags = soup('td')
+
+tags = soup('span')
 for tag in tags:
-    y = re.findall('[0-9]+',tag.decode())
-    if len(y) > 0 :
-        numbers.append(y)
-print (numbers)
+    str = re.findall('[0-9]+',tag.decode())
+    if len(str) > 0 :
+        numbers.append(str)
+
+#To convert list of list to list of numbers
+counter = list()
+for i in numbers :
+    for j in i :
+        counter.append(j)
+# Sum of all the numbers in list counter
 sum = 0
-
-#for number in numbers :
-#    if number > 0 :
-#        sum = sum + int(number)
-
-print (dir(numbers))
+for number in counter :
+    sum = sum + int(number)
 print (sum)
