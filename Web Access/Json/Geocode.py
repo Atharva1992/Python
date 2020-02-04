0a74c41da52b25af7e8bfca42be942d7fdbb9f9b
Atharva1992 <atharvakekare@gmail.com>
2020-02-04 15:09:43 +0530

    Geocode fixed

---
+++ Web Access/Json/Geocode.py
@@ -0,0 +1,31 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+address = input("Enter the Address : ")
+
+urlinit = "https://maps.googleapis.com/maps/api/geocode/json?"
+key = "AIzaSyBBSQ2ymnmRgHtuR-KGxAOhJcYY_EOqrb4"
+parm = dict()
+parm['address'] = address
+parm['key'] = "AIzaSyBBSQ2ymnmRgHtuR-KGxAOhJcYY_EOqrb4"
+
+
+url = urlinit + urllib.parse.urlencode(parm)
+jsondata = urllib.request.urlopen(url)
+
+data = jsondata.read().decode()
+
+#print(data)
+
+try :
+    js = json.loads(data)
+except :
+    js = None
+
+if not js or 'status' not in js or js['status'] != 'OK' :
+    print("Failure to retrieve data")
+
+lat = js["results"][0]["geometry"]["bounds"]["northeast"]["lat"]
+lng = js["results"][0]["geometry"]["bounds"]["northeast"]["lng"]
+
+print(lat,lng)
